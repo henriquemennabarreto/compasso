9021735470065787717010102451f62185c7b8ec
Henrique Menna Barreto <henrique.menna@triider.com>
2021-07-07 02:38:07 -0300

    user module

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
+
+export default User;
